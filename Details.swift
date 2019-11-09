0d7d56eadcfe6c4709407fb996fbd3e3e8dc1fdc
snowsoar <livefei@live.com>
2019-11-09 17:54:53 +0800

    为图片添加点击事件，并添加简单动画
    点击图片进行缩放，添加动画效果

--- Details.swift
+++ Details.swift
@@ -8,21 +8,34 @@
 
 import SwiftUI
 
+/*
+ struct 不能改变
+ @State 属性可以改变,一旦被改变将会自动刷新 body ,不需要 reload
+ */
 struct Details: View {
-    var model:Landmark  // 定义一个常量,类型为Landmark
+    let model:Landmark  // 定义一个常量,类型为Landmark
+    @State var zoomed = false  // 定义一个变量，用来改变图像 displayMode
     var body: some View {
-        ZStack(alignment:.bottomTrailing){ //前后层叠视图（后写的在最上层）
+        ZStack(alignment:.bottomTrailing){ // 前后层叠视图（后写的在最上层）
             Image(model.imageName)
-                .resizable()    //自动调整图片大小
-                .scaledToFit()  //图像显示模式
+                .resizable()    // 自动调整图片大小
+                .aspectRatio(contentMode: zoomed ? .fill : .fit) // 图像显示模式
                 .frame(minWidth: 0, idealWidth: 0, maxWidth: .infinity, minHeight: 0, idealHeight: 0, maxHeight: .infinity)
                 .navigationBarTitle(Text(model.name),displayMode: .inline)
-            
-            Text(model.location)
-                .font(.largeTitle)
-                .foregroundColor(.secondary)
-                .padding()//默认边距
+                .onTapGesture { //点击事件 回调
+                    withAnimation(.default) { // 动画
+                        self.zoomed.toggle()    // Bool类型值自动切换 false、true
+                    }
+                    // 值改变后不需要 reload，系统会自动刷新
+            }
+            if !zoomed{
+                Text(model.location)
+                    .font(.largeTitle)
+                    .foregroundColor(.secondary)
+                    .padding()//默认边距
+            }
         }
+        
     }
 }
 
